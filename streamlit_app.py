442cc0b528d7f3e829ca4538fd21b4fcc04ce432
Eduard <147880628+edilavric@users.noreply.github.com>
2026-03-23 19:58:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 from snowflake.snowpark.functions import when_matched
-
+import requests 
 
 # Toate liniile încep exact de la marginea din stânga
 
@@ -33,6 +33,8 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
@@ -44,9 +46,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered,'+name_on_order+'!',icon="✅")
 
-import requests  
+ 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response.json())
 sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
